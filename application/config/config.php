4de277e7572e78140065cd3d2d8da58d0be6c643
muhfahmir <fahmi.rahmat07@gmail.com>
2021-02-22 20:05:28 +0700

    handling session error

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci3-project/ci3komik/';
+// $config['base_url'] = 'http://localhost/ci3-project/ci3komik/';
+$config['base_url'] = 'https://ci3komik.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
